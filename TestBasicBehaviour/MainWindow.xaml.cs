4b62e868f94f6c2f239ae6a9674567437626bd18
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-22 03:13:14 +0200

    Add project files.

---
+++ TestBasicBehaviour/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿/// Add Nuget System.Windows.Interactivity.WPF
+
+
+using System.Windows;
+
+namespace TestBasicBehaviour
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        public double Translucent
+        {
+            get => (double)GetValue(TranslucentProperty);
+            set => SetValue(TranslucentProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Translucent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TranslucentProperty =
+            DependencyProperty.Register("Translucent", typeof(double), typeof(MainWindow), new PropertyMetadata(1.0));
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
